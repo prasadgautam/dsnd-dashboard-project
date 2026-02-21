534f56370cf278c31b75f70e7b950b2cbde50699
Gautam Prasad <68378720+prasadgautam@users.noreply.github.com>
2026-02-21 10:06:58 +0000

    Update employee.py

--- python-package/employee_events/employee.py
+++ python-package/employee_events/employee.py
@@ -1,48 +1,57 @@
 # Import the QueryBase class
-#### YOUR CODE HERE
+from .query_base import QueryBase
 
 # Import dependencies needed for sql execution
 # from the `sql_execution` module
-#### YOUR CODE HERE
+from .sql_execution import query
 
 # Define a subclass of QueryBase
 # called Employee
-#### YOUR CODE HERE
+class Employee(QueryBase):
 
     # Set the class attribute `name`
     # to the string "employee"
-    #### YOUR CODE HERE
-
+    name = "employee"
 
     # Define a method called `names`
     # that receives no arguments
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
-        
+    @query
+    def names(self):
+
         # Query 3
         # Write an SQL query
-        # that selects two columns 
+        # that selects two columns
         # 1. The employee's full name
         # 2. The employee's id
         # This query should return the data
         # for all employees in the database
-        #### YOUR CODE HERE
-    
+        return f"""
+            SELECT first_name || ' ' || last_name AS full_name,
+                   employee_id
+            FROM {self.name}
+            ORDER BY last_name, first_name
+        """
 
     # Define a method called `username`
     # that receives an `id` argument
     # This method should return a list of tuples
     # from an sql execution
-    #### YOUR CODE HERE
-        
+    @query
+    def username(self, id):
+
         # Query 4
         # Write an SQL query
         # that selects an employees full name
         # Use f-string formatting and a WHERE filter
         # to only return the full name of the employee
         # with an id equal to the id argument
-        #### YOUR CODE HERE
+        return f"""
+            SELECT first_name || ' ' || last_name AS full_name
+            FROM {self.name}
+            WHERE employee_id = {id}
+        """
 
 
     # Below is method with an SQL query
@@ -55,7 +64,8 @@
     #### YOUR CODE HERE
     def model_data(self, id):
 
-        return f"""
+        return self.pandas_query(
+            f"""
                     SELECT SUM(positive_events) positive_events
                          , SUM(negative_events) negative_events
                     FROM {self.name}
@@ -63,3 +73,4 @@
                         USING({self.name}_id)
                     WHERE {self.name}.{self.name}_id = {id}
                 """
+        )
